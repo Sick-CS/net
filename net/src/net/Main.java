52153e7739c9c92dd568c2329a4e80c0dd684a60
ASUS <ASUS@DESKTOP-3H31FAT>
2024-11-19 10:50:22 +0530

    my commit

---
+++ net/src/net/Main.java
@@ -0,0 +1,17 @@
+package net;
+
+class A {
+
+	void meth() {
+		System.out.println("meth");
+
+	}
+
+}
+
+public class Main {
+	public static void main(String[] args) {
+		A a = new A();
+		a.meth();
+	}
+}
