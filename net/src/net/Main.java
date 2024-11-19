d6075e769554d14655d0c84d3a3089432262eb94
ASUS <ASUS@DESKTOP-3H31FAT>
2024-11-19 10:53:18 +0530

    updated

--- net/src/net/Main.java
+++ net/src/net/Main.java
@@ -4,11 +4,9 @@ class A {
 
 	void meth() {
 		System.out.println("meth");
-
+		System.out.println("meth1");
 	}
-
 }
-
 public class Main {
 	public static void main(String[] args) {
 		A a = new A();
